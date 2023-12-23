e68a4bc58a2ac4e6ba7f49757e385a84936c8e75
olimov Amir <olimov.amir@mail.ru>
2023-12-23 12:16:39 +0300

    Добавьте файлы проекта.

---
+++ Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_Set
+{
+    internal class Point
+    {
+        private int X;
+
+        public void SetX(int X)
+        {
+            if (X < 1)
+            {
+                this.X = 1;
+                return;
+            }
+            if (X > 5)
+            {
+                this.X = 5;
+                return;
+            }
+            this.X = X;
+        }
+
+        public int GetX()
+        {
+            return X;
+        }
+
+        private int y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Get_Set.Point point = new Get_Set.Point();
+
+        point.SetX(15);
+        int valueX =  point.GetX();
+        Console.WriteLine(valueX);
+
+        point.Y = 25;
+        int valueY = point.Y;
+        Console.WriteLine(valueY);
+    }
+}
